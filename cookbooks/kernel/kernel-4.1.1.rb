daffe5cab038b4f9c2ff11f0fcfb510952bce9b5
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-07-15 00:59:57 +0900

    Move kernel image to user directory

--- cookbooks/kernel/kernel-4.1.1.rb
+++ cookbooks/kernel/kernel-4.1.1.rb
@@ -1,9 +1,26 @@
+node ||= {
+  "rbenv" => {
+    "versions" => [ "2.2.1", "2.2.2" ],
+    "local" => "2.2.2",
+    "gems" => [ "bundler", "rake" ],
+    "user" => "vagrant",
+    "rbenv_config_file" => "~/.bash_profile",
+    "rbenv_dir" => "/var/tmp/rbenv",
+    "rubybuild_dir" => "/var/tmp/rbenv/plugins/ruby-build",
+  },
+}
+
 build_dir = "/usr/local/src/kernel-build"
 
 git build_dir do
   repository "https://github.com/matsumoto-r/build-kernel-4.x-for-centos6.git"
 end
 
+execute "setup building kernel" do
+  command "chown -R #{node["rbenv"]["user"]}.#{node["rbenv"]["user"]} #{build_dir}"
+end
+
 execute "build kernel" do
-  command "cd #{build_dir} && make"
+  user node["rbenv"]["user"]
+  command "cd #{build_dir} && make && mv #{build_dir}/build/linux-4.1.1 ~/."
 end
