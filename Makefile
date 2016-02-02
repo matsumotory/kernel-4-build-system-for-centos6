20f75cb0d638036dbdf5e0fccde3409f8ac32820
matsumoto-r <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-02-02 21:06:55 +0900

    Support kernel build parameters like build host, user and prefix

--- Makefile
+++ Makefile
@@ -5,13 +5,16 @@ PWD=$(shell pwd)
 
 KERNEL_VER=$(shell curl -s https://www.kernel.org | grep stable: -A1 | tail -1 | perl -nle '/>([^<]+)</; print $$1')
 BUILD_DIR=$(PWD)/build/linux-$(KERNEL_VER)
+KERNEL_BUILD_HOST=
+KERNEL_BUILD_USER=
+KERNEL_LOCAL_VER=
 
 all: itamae
 
 stable: itamae
 
 itamae: vagrant
-	KERNEL_VER=$(KERNEL_VER) bundle exec itamae ssh -l debug --vagrant roles/build-kernel-4.x.y-for-centos6.rb
+	KERNEL_VER=$(KERNEL_VER) KBUILD_BUILD_HOST=$(KERNEL_BUILD_HOST) KBUILD_BUILD_USER=$(KERNEL_BUILD_USER) KERNEL_LOCAL_VER=$(KERNEL_LOCAL_VER) bundle exec itamae ssh -l debug --vagrant roles/build-kernel-4.x.y-for-centos6.rb
 
 vagrant:
 	vagrant up || vagrant plugin install vagrant-persistent-storage
--- cookbooks/kernel/kernel-4.x.y.rb
+++ cookbooks/kernel/kernel-4.x.y.rb
@@ -12,6 +12,9 @@ node ||= {
 
 build_dir = "/usr/local/src/kernel-build"
 version = ENV["KERNEL_VER"].nil? ? "4.1.1" : ENV["KERNEL_VER"]
+host = ENV["KERNEL_BUILD_HOST"].nil? ? "" : ENV["KERNEL_BUILD_HOST"]
+user = ENV["KERNEL_BUILD_USER"].nil? ? "" : ENV["KERNEL_BUILD_USER"]
+prefix = ENV["KERNEL_LOCAL_VER"].nil? ? "" : ENV["KERNEL_LOCAL_VER"]
 
 git build_dir do
   repository "https://github.com/matsumoto-r/build-kernel-4.x-for-centos6.git"
@@ -23,5 +26,5 @@ end
 
 execute "build kernel" do
   user node["rbenv"]["user"]
-  command "cd #{build_dir} && make THREAD=2 KERNEL_VER=#{version} && cp -prfv #{build_dir}/build/linux-#{version} /vagrant/build/. "
+  command "cd #{build_dir} && make THREAD=2 KERNEL_VER=#{version} KERNEL_BUILD_HOST=#{host} KERNEL_BUILD_USER=#{user} KERNEL_LOCAL_VER=#{prefix} && cp -prfv #{build_dir}/build/linux-#{version} /vagrant/build/. "
 end
